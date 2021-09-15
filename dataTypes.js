57406b6bda46a93b7abcdbd747c2b3dd1a458031
mollycabs <89987495+mollycabs@users.noreply.github.com>
2021-09-15 18:11:19 -0400

    Add files via upload

---
+++ dataTypes.js
@@ -0,0 +1,36 @@
+var number1 = 4; //javascript only has one type of number
+var number2 = 4.2; //numbers can be written with or without decimals
+var string1 = "Hey :)"; //strings are initialized using quotes
+var happy = false;
+var kardashians = ["Kim", "Khloe", "Kourtney"]; //arrays use square brackets
+var shirt = {color:"Blue", sleeves:"Short", occasion:"Casual"}; //objects use curly brackets
+var banana; //when a variable is undefined, the type is undefined
+
+console.log(number1);
+console.log(number2);
+console.log(happy);
+console.log(kardashians);
+console.log(shirt);
+console.log(banana);
+
+var number1 = "hi!!!!"; //variables can hold different data types and are mutable
+
+var things = [1,"banana",true];
+console.log(things); //arrays can hold different data types!
+
+var x = "5" + 6;
+console.log(x); //this runs and returns 56 because javascript concatenates strings when they are added.
+//only one of the values being added needs to be a string for this to happen, as seen here
+
+let y = 2; //let is another way to instantiate a variable
+            //this method creates a variable that cannot be redeclared
+//let y = 4; 
+//this would give an error message since y has already been declared.
+//variables must also be declared before use
+
+const z = 1; //const is another way to instantiate a variable
+                //this creates a variable that can't be redeclared or reassigned 
+//const z = z +1 //this will not work 
+
+
+
